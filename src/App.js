17646423a23706697bc6b839028247fc1bf608ca
Ha Rim Ku <hrku19@gmail.com>
2020-08-20 00:31:11 -0700

    Connecting React client to Express API

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    //initialize default state
+    constructor(props) {
+        super(props);
+        this.state = { apiResponse: "" };
+    }
+    
+    //fetch data from the API and store the response to 'this.state.apiResponse'
+    callAPI() {
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }));
+    }
+    
+    //React lifecycle method; executes callAPI method after the component mounts
+    componentWillMount() {
+        this.callAPI();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">;{this.state.apiResponse}</p>
+            </div>
+        );
+    }
 }
 
 export default App;
